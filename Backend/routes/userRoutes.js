43c519d72f97b20d03a6e9bb5cf35e627f28bd3d
deasejdiu <ds64830@ubt-uni.net>
2024-06-26 22:11:16 +0200

    Logout

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authUser, deleteUser, getUserById, getUsers, registerUser, updateUser } from "../controllers/userController.js";
+import { authUser, deleteUser, getUserById, getUsers, logoutUser, registerUser, updateUser } from "../controllers/userController.js";
 import { protectUser } from "../middleware/authMiddleware.js";
 
 
@@ -7,6 +7,7 @@ import { protectUser } from "../middleware/authMiddleware.js";
 const router = new express.Router();
 
 router.post("/login", authUser);
+router.post("/logout", logoutUser);
 router.post("/", registerUser);
 router.get("/all", protectUser, getUsers);
 router.get("/get/:id", protectUser, getUserById);
